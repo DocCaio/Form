98864829b037ffdfa24b15169b333586500d384a
Caio <108128119+DocCaio@users.noreply.github.com>
2024-06-17 17:36:01 -0300

    up

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,18 @@
+import './Form.module.css';
+
+const Form = () => {
+    return (    
+    <main>
+        <div  className="form-container">
+        <h1>Contact Us</h1>
+            <form>
+                <fieldset>
+                    
+                </fieldset>
+            </form>
+        </div>
+    </main>
+    );
+  };
+  
+  export default Form ;
