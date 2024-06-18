6bd6dfaa03d7145290d17f183f1a5e1f1d0c1805
Caio <108128119+DocCaio@users.noreply.github.com>
2024-06-18 09:23:26 -0300

    UP

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import './Button.module.css';
+
+const Button = () => {
+    return (
+        <button type="submit">Submit</button>
+    )
+
+}
+
+export default Button;
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,18 +1,11 @@
+
 import './Form.module.css';
 
-const Form = () => {
-    return (    
-    <main>
-        <div  className="form-container">
-        <h1>Contact Us</h1>
-            <form>
-                <fieldset>
-                    
-                </fieldset>
-            </form>
-        </div>
-    </main>
+ export default  function Form () {
+    return ( 
+      <div>
+
+      </div>
     );
   };
   
-  export default Form ;
